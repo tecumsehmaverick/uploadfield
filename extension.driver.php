60d42103548c0bea00d20022b6d86ab3ae9e2395
Rowan Lewis <rowan@randb.com.au>
2010-02-26 12:42:48 +1000

    Upload extension ready.

--- extension.driver.php
+++ extension.driver.php
@@ -1,31 +1,31 @@
 <?php
 	
-	class Extension_AdvancedUploadField extends Extension {
+	class Extension_UploadField extends Extension {
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
 		
 		public function about() {
 			return array(
-				'name'			=> 'Field: Advanced Upload',
-				'version'		=> '1.0.6',
-				'release-date'	=> '2009-09-09',
+				'name'			=> 'Field: Upload',
+				'version'		=> '2.0.0',
+				'release-date'	=> '2010-02-16',
 				'author'		=> array(
 					'name'			=> 'Rowan Lewis',
 					'website'		=> 'http://rowanlewis.com/',
 					'email'			=> 'me@rowanlewis.com'
 				),
-				'description'	=> 'An enhanced upload field with image preview.'
+				'description'	=> 'An upload field that allows features to be plugged in.'
 			);
 		}
 		
 		public function uninstall() {
-			$this->_Parent->Database->query("DROP TABLE `tbl_fields_advancedupload`");
+			$this->_Parent->Database->query("DROP TABLE `tbl_fields_upload`");
 		}
 		
 		public function install() {
 			$this->_Parent->Database->query("
-				CREATE TABLE IF NOT EXISTS `tbl_fields_advancedupload` (
+				CREATE TABLE IF NOT EXISTS `tbl_fields_upload` (
 					`id` int(11) unsigned NOT NULL auto_increment,
 					`field_id` int(11) unsigned NOT NULL,
 					`destination` varchar(255) NOT NULL,
@@ -35,6 +35,8 @@
 					KEY `field_id` (`field_id`)
 				)
 			");
+			
+			// TODO: Upgrade existing table
 			
 			return true;
 		}
@@ -47,8 +49,8 @@
 		
 		public function addHeaders($page) {
 			if (!$this->addedHeaders) {
-				$page->addStylesheetToHead(URL . '/extensions/advanceduploadfield/assets/publish.css', 'screen', 9745190);
-				$page->addScriptToHead(URL . '/extensions/advanceduploadfield/assets/publish.js', 9745190);
+				$page->addStylesheetToHead(URL . '/extensions/uploadfield/assets/publish.css', 'screen', 9745190);
+				$page->addScriptToHead(URL . '/extensions/uploadfield/assets/publish.js', 9745190);
 				
 				$this->addedHeaders = true;
 			}
